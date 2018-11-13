779e8566b0861ca0148c4928d73c8440e59ef78a
Evoxius <37443179+Evoxius@users.noreply.github.com>
2018-11-13 10:02:33 +0100

    Convert links
    changed all links from healthone.test/SOMETHING to localhost/healthone/public/SOMETHING for availability across multiple computers

--- public/js/processInfo.js
+++ public/js/processInfo.js
@@ -1,27 +1,27 @@
 function process()
 {
-var url1="http://healthone.test/medicijn/" + document.getElementById("url1").value;
+var url1="http://localhost/healthone/public/medicijn/" + document.getElementById("url1").value;
 location.href=url1;
 return false;
 }
 
 function process2()
 {
-var url2="http://healthone.test/arts/" + document.getElementById("url2").value;
+var url2="http://localhost/healthone/public/arts/" + document.getElementById("url2").value;
 location.href=url2;
 return false;
 }
 
 function process3()
 {
-var url3="http://healthone.test/recept/" + document.getElementById("url3").value;
+var url3="http://localhost/healthone/public/recept/" + document.getElementById("url3").value;
 location.href=url3;
 return false;
 }
 
 function process4()
 {
-var url4="http://healthone.test/patient/" + document.getElementById("url4").value;
+var url4="http://localhost/healthone/public/patient/" + document.getElementById("url4").value;
 location.href=url4;
 return false;
 }
---
+++ public/js/processInfoArts.js
@@ -0,0 +1,6 @@
+function process2()
+{
+var url2="http://localhost/healthone/public/arts/" + document.getElementById("url2").value;
+location.href=url2;
+return false;
+}
