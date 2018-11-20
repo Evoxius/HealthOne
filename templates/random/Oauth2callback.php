63d7d5fa7bb968907201d1c94811bdc1bf44591c
Evoxius <37443179+Evoxius@users.noreply.github.com>
2018-11-20 18:21:28 +0100

    google API success

--- templates/random/Oauth2callback.php
+++ templates/random/Oauth2callback.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '../../../vendor/autoload.php';
+require  '../../vendor/autoload.php';
 
 session_start();
 
@@ -7,9 +7,9 @@ $client = new Google_Client();
 
 $client->setClientId("542936429191-eu265aphvc3m2967t9np9jvsmq2op16t.apps.googleusercontent.com");
 $client->setClientSecret("D4QptYuFW1M-Tdh-ocBAHL2m");
-$client->setRedirectUri('https://localhost/oauth/oauth2callback.php');
+$client->setRedirectUri('http://localhost/healthone/templates/random/oauth2callback.php');
 
-$client->addScope("https://wwww.googleapis.com/auth/drive.readonly");
+
 $client->addScope("https://www.googleapis.com/auth/books");
 $client->addScope("email");
 $client->addScope("profile");
@@ -20,6 +20,6 @@ if (!isset($_GET['code'])) {
 } else {
     $client->fetchAccessTokenWithAuthCode($_GET['code']);
     $_SESSION['access_token'] = $client->getAccessToken();
-    $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth/index.php';
+    $redirect_uri = 'http://localhost/healthone/templates/random/index.php';
     header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
--- templates/random/index.php
+++ templates/random/index.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '../../../vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 session_start();
 
@@ -8,7 +8,7 @@ $client = new Google_Client();
 $client->setClientId("542936429191-eu265aphvc3m2967t9np9jvsmq2op16t.apps.googleusercontent.com");
 $client->setClientSecret("D4QptYuFW1M-Tdh-ocBAHL2m");
 
-$client->addScope("https://wwww.googleapis.com/auth/drive.readonly");
+
 $client->addScope("https://www.googleapis.com/auth/books");
 $client->addScope("email");
 $client->addScope("profile");
@@ -26,15 +26,8 @@ if (isset($_SESSION['access_token']) && !empty($_SESSION['access_token']))
         echo "<h3>".$item['volumeInfo']['title']."</h3>";
         echo $item['volumeInfo']['description']."</br>";
     }
-    $drive = new Google_Service_Drive($client);
-    $files = $drive->files->listFiles(array())->getFiles();
-    echo "<h2>Mijn bestanden</h2>";
-    foreach($files as $file)
-    {
-        echo "<br/>";
-        echo"<h4>".$file['name']."</h4>";
-    }
+    
 } else {
-    $redirect_url = 'https://'.$_SERVER['HTTPS_HOST']. '/oauth/oauth2callback.php';
+    $redirect_url = 'http://localhost/healthone/templates/random/oauth2callback.php';
     header('Location:'.filter_var($redirect_url, FILTER_SANITIZE_URL));
 }
