48f145d41404d0de3f8a17cd8a808c9b6578bb5a
Evoxius <37443179+Evoxius@users.noreply.github.com>
2018-11-29 09:46:45 +0100

    Permission fixes

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomepageController extends AbstractController
 {
@@ -14,9 +15,15 @@ class HomepageController extends AbstractController
      */
     public function index()
     {
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
+
+        /** @var \App\Entity\Arts $arts */
+        $arts = $this->getUser();
+
+        return new Response('<html><body>Hello</body></html> '.$arts->getVoornaam());
         
     }
 }
